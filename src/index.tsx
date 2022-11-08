a485dfc583173efb4385a65015235b0d3a735b0e
georgei <george.iuriet@adyen.com>
2022-11-08 16:23:07 +0100

    Validate target in the constructor and remove IE 11 from browsers list

--- src/index.tsx
+++ src/index.tsx
@@ -4,22 +4,24 @@ if (process.env.NODE_ENV === 'development') {
 import { h, render } from 'preact';
 import DocumentViewer from './DocumentViewer';
 
-type target = HTMLElement | string;
-
 export default class AdyenDocumentViewer {
-    private readonly target: target;
+    private readonly target: HTMLElement;
 
     /**
      * Initializes the library
      * @param target - The target element (HTMLElement or querySelector string)
      */
-    constructor(target: target) {
-        this.target = target;
-    }
-
-    private renderViewer(target: target, document: JSON): void {
-        const targetElement = typeof target === 'object' && 'nodeType' in target ? target : window.document.querySelector(target);
-        render(<DocumentViewer document={document} />, targetElement);
+    constructor(target: HTMLElement | string) {
+        if (typeof target === 'object' && 'nodeType' in target) {
+            this.target = target;
+        } else {
+            try {
+                this.target = window.document.querySelector(target);
+                if (!this.target) throw Error('Target element was not found');
+            } catch (e) {
+                throw Error(e);
+            }
+        }
     }
 
     /**
@@ -27,6 +29,6 @@ export default class AdyenDocumentViewer {
      * @param document - The JSON document
      */
     render(document: JSON): void {
-        this.renderViewer(this.target, document);
+        render(<DocumentViewer document={document} />, this.target);
     }
 }
