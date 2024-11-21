581cf05a9b24445f4c16a57ca636599513f184f9
Alex Tompkins <alex.tompkins@adyen.com>
2024-11-21 14:35:23 +0100

    Add Playwright tests for the built package to ensure that both import methods work

---
+++ playwright.config.ts
@@ -0,0 +1,44 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
+
+const IS_CI = Boolean(process.env.CI);
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+const config: PlaywrightTestConfig = {
+  testDir: './tests',
+  timeout: 5 * 1000,
+  expect: {
+    timeout: 1000,
+  },
+  fullyParallel: true,
+
+  forbidOnly: IS_CI,
+  /* Retry on CI only. Playwright will tell us if a test is flaky */
+  retries: IS_CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: IS_CI ? 1 : undefined,
+
+  reporter: 'html',
+  use: {
+    baseURL: 'http://localhost:5174/',
+    trace: 'on-first-retry',
+    headless: IS_CI,
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: devices['Desktop Chrome'],
+    },
+  ],
+
+  webServer: {
+    command: 'npm run build && npm start',
+    port: 5174,
+    reuseExistingServer: !IS_CI,
+  },
+};
+
+export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,44 @@
 import { setDefaultResultOrder } from 'node:dns';
 import { resolve } from 'node:path';
-import { defineConfig } from 'vite';
+
 import preact from '@preact/preset-vite';
+import { defineConfig } from 'vite';
 
 // This forces Vite to use `localhost` instead of `127.0.0.1`. Otherwise, we run into CORS issues
 // since `localhost:8080` and `localhost:8082` are same-origin, but `127.0.0.1:8082` isn't.
 setDefaultResultOrder('verbatim');
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
-  return {
-    css: {
-      modules: {
-        // TODO: Remove this after changing all of our SCSS @import to @use
-        scopeBehaviour: 'local',
-        generateScopedName: (name) => name,
+export default defineConfig(() => ({
+  root: resolve(__dirname, 'playground'),
+  publicDir: resolve(__dirname, 'dist'),
+  css: {
+    modules: {
+      // TODO: Remove this after changing all of our SCSS @import to @use
+      scopeBehaviour: 'local',
+      generateScopedName: (name) => name,
+    },
+  },
+  build: {
+    lib: {
+      name: 'AdyenDocumentViewer',
+      entry: resolve(__dirname, 'src/index.tsx'),
+      formats: ['es', 'umd'],
+      fileName: (format) =>
+        format === 'es' ? 'adyen-document-viewer.min.mjs' : 'adyen-document-viewer.min.js',
+    },
+    rollupOptions: {
+      output: {
+        assetFileNames: 'adyen-document-viewer.min.[ext]',
       },
     },
-    build: {
-      lib: {
-        name: 'AdyenDocumentViewer',
-        entry: resolve(__dirname, 'src/index.tsx'),
-        formats: ['es', 'umd'],
-        fileName: (format) =>
-          format === 'es' ? 'adyen-document-viewer.min.mjs' : 'adyen-document-viewer.min.js',
-      },
-      rollupOptions: {
-        output: {
-          assetFileNames: 'adyen-document-viewer.min.[ext]',
-        },
-      },
-      minify: true,
-      outDir: resolve(__dirname, 'dist'),
-      emptyOutDir: true,
-    },
-    plugins: [preact()],
-  };
-});
+    minify: true,
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+  },
+  plugins: [preact()],
+  server: {
+    host: 'localhost',
+    port: 5174,
+  },
+}));
